9ec834125925c0744fa4178cf9eaa7e838d31a4f
Natayra Pacheco dos Santos <natayras@gmail.com>
2020-09-06 21:25:16 +0200

    m

--- frontend/src/components/Login/index.js
+++ frontend/src/components/Login/index.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useHistory, useParams } from 'react-router-dom';
+import { useHistory} from 'react-router-dom';
 
 import PageTitle from '../Shared/PageTitle';
 import { BaseContentButton } from '../../style/buttons';
